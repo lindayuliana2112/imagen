a2bf3d04808217338cc833fefb462466ee9050d0
lindayuliana2112 <124885958+lindayuliana2112@users.noreply.github.com>
2023-08-26 22:05:37 -0500

    Add files via upload

---
+++ hoja de vida/hoja_de_vida.js
@@ -0,0 +1,6 @@
+document.getElementById("btnPasajes").addEventListener("click", function() {
+  
+ 
+    window.location.href = "https://docs.google.com/document/d/1e8KIcVq3qRxSvJcanH1biZ2I7_TzvwGF/edit?usp=sharing&ouid=112920125168535308826&rtpof=true&sd=true";
+    });
+   
